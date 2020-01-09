db9cba8cb096daab205bcc7a9b61667cbea61640
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-08 20:37:46 -0500

    changed endpoint, again. /api  got rid of /things

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://shrouded-beach-61234.herokuapp.com/api/things',
+  API_ENDPOINT: 'https://shrouded-beach-61234.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
